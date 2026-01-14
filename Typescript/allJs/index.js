991a4d32b72d3d4908e33554694d616cb600a728
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2026-01-14 22:10:25 +0530

    created new typescript folder

--- Typescript/allJs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getInfo() {
-    let userNameInput = document.getElementById("userName");
-    const userName = userNameInput.value;
-    console.log("====================================");
-    console.log(userName);
-    console.log("====================================");
-}
