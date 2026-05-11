77f5c6880abbbd6d0cad85b397d072ffe2a329ec
AartiGhorpade <asghorpade99@gmail.com>
2026-05-11 22:48:42 +0530

    more programs

---
+++ Javascript/DSA-Akshay-Saini/Recursion/Factorial.js
@@ -0,0 +1,20 @@
+// let fact = 1;
+
+// function factorial(n) {
+//   if (n == 0) return 1;
+
+//   fact *= n;
+
+//   factorial(n - 1);
+//   return fact;
+// }
+
+// console.log(factorial(5));
+
+function factorial(n) {
+  if (n == 0) return 1;
+
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
